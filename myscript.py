04a2ed4c06651b0b9253c77385e2eec6bbd4882c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-08-27 12:16:18 +0200

    Displays and comments

--- myscript.py
+++ myscript.py
@@ -1,1 +1,14 @@
 print("Hello, World!")
+print(3+5)
+# This is a single-line comment
+print("Python is fun")
+"""
+This is a multi-line comment
+describing the code below
+"""
+print("Multi-line comment above")
+'''
+This is another multi-line comment
+describing the code below
+'''
+print("Another multi-line comment above")
