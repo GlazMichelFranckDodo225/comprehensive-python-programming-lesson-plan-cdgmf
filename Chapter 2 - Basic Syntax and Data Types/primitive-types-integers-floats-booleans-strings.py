432c9140feb5b5b0d0f3db267452b1b172b222aa
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-11-13 08:22:28 +0100

    Primitive data types: Integers, Floating-point numbers, Booleans, Strings

---
+++ Chapter 2 - Basic Syntax and Data Types/primitive-types-integers-floats-booleans-strings.py
@@ -0,0 +1,29 @@
+# Integers (int)
+# Whole numbers, positive or negative
+from tkinter.font import names
+
+x = 10
+y = -5
+print(type(x))
+print(type(y))
+
+# Floats (float)
+# Numbers with decimals
+pi = 3.14
+temp = -2.5
+print(type(pi))
+print(type(temp))
+
+# Booleans (bool)
+# Logical values: True or False
+is_active = True
+is_logged_in = False
+print(type(is_active))
+print(type(is_logged_in))
+
+# Strings (str)
+# Sequence of characters, enclosed in quotes (' ' or " ")
+greeting = "Hello"
+name = "Michael"
+print(type(greeting))
+print(type(name))
