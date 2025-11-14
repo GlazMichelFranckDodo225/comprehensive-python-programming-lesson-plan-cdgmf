b4c6780ce8cf8447a16f4735536bd57bbd525c78
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-11-14 06:37:39 +0100

    String Basics (Concatenation, Formatting, f-strings)

---
+++ Chapter 2 - Basic Syntax and Data Types/string-basics-concatenation-formatting-f-strings.py
@@ -0,0 +1,19 @@
+# Concatenation (+)
+first = "Python"
+second = "is making waves"
+print(first + " " + second) # Python is making waves
+
+# Repetition (*)
+print("Hi! " * 3)
+
+# Old formatting (%)
+name = "Alice"
+age = 25
+print("Name: %s, Age: %d" %(name, age))
+
+# str.format() method
+print("Name: {}, Age: {}".format(name, age))
+
+# f-strings (Python 3.6+)
+# Preferred modern method.
+print(f"My name is {name} and I am {age} years old")
