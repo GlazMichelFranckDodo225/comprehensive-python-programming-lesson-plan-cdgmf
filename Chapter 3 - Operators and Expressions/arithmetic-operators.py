167032b5d6c5ce4cf41a346be67bc460700db3fc
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-11-15 07:16:22 +0100

    Arithmetic Operators

---
+++ Chapter 3 - Operators and Expressions/arithmetic-operators.py
@@ -0,0 +1,38 @@
+# Arithmetic
+# +, -, *, /, //, %, **
+
+# addition
+addition = 7 + 2
+print(addition) # 9
+
+# substraction
+subtraction = 7 - 2
+print(subtraction) # 5
+
+# multiplication
+multiplication = 7 * 2
+print(multiplication) # 14
+
+# / : always returns float
+slash = 7 / 2
+print(slash) # 3.5
+print(type(slash)) # float
+slash_2 = 8 / 4
+print(slash_2) # 2.0
+print(type(slash_2)) # float
+
+# // : floor-division
+floor_division = 7 // 2
+print(floor_division) # 3
+print(type(floor_division)) # int
+
+# % : remainder
+remainder = 7 % 2
+print(remainder) # 1
+print(type(remainder)) # int
+
+# ** : exponent (right-associative)
+exponent = 2 ** 3 ** 2
+print(exponent) # 2 ** (3 ** 2) = 512
+
+
